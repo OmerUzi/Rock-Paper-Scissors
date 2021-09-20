f8ab3c6f50aa090bbe3e57cd56aee3fbbe5e1345
omer uzi <omer135798@gmail.com>
2021-09-20 16:12:33 +0300

    create ui for rps

--- script.js
+++ script.js
@@ -8,48 +8,66 @@ function computerPlay(){
         case 2:
             return "scissors";
    }
-
-
 }   
-function playRound(playerSelection,computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    if (computerSelection == playerSelection ) return "it's a draw, you both play " + playerSelection;
+function playRound(event){
+    playerSelection = event.target.textContent;
+    computerSelection = computerPlay()
+    playerSelection = playerSelection.toLowerCase(); 
+    if (computerSelection == playerSelection )  h1.textContent = ("it's a draw, you both play " + playerSelection);
     else{
-        if (playerSelection == "rock" && computerSelection == "paper") return  "You lose! paper beats rock"; 
-        else if (playerSelection == "rock" && computerSelection == "scissors") return  "You win! Rock beats scissors";
-        else if (playerSelection == "paper" && computerSelection == "rock") return  "You win! Paper beats Rock";
-        else if (playerSelection == "paper" && computerSelection == "scissors") return  "You lose! scissors beats paper";
-        else if (playerSelection == "scissors" && computerSelection == "paper") return  "You win! scissors beats paper";
-        else if (playerSelection == "scissors" && computerSelection == "rock") return  "You lose! Rock beats scissors";
-        else return "There is not an option " + playerSelection;
+        if (playerSelection == "rock" && computerSelection == "paper")  {
+            h1.textContent = ("You lose! paper beats rock"); 
+            computerresult++;
+        }
+        else if (playerSelection == "rock" && computerSelection == "scissors")  {  
+            h1.textContent = ("You win! Rock beats scissors");
+            playerresult++;
+        }
+        else if (playerSelection == "paper" && computerSelection == "rock") {
+            h1.textContent = ("You win! Paper beats Rock");
+            playerresult++;
+        }
+        else if (playerSelection == "paper" && computerSelection == "scissors") { 
+            h1.textContent = ("You lose! scissors beats paper");
+            computerresult++;
+        }
+        else if (playerSelection == "scissors" && computerSelection == "paper") {  
+            h1.textContent = ("You win! scissors beats paper");
+            playerresult++;
+        }
+        else if (playerSelection == "scissors" && computerSelection == "rock"){   
+            h1.textContent =("You lose! Rock beats scissors");
+            computerresult++;
+        }
+        else  h1.textContent = ("There is not an option " + playerSelection);
+        
+        if(playerresult == 5){
+            alert("total win!");
+            playerresult = 0;
+            computerresult = 0;
+        }
+        if(computerresult == 5){
+            alert("total loss!")
+            playerresult = 0;
+            computerresult = 0;
+        }
+        playerh3.textContent = "player: " + playerresult;
+        computerh3.textContent = "computer: " + computerresult;
+        
+        }
+
     }
     
-}
+    
 
-function game(){
-    let playersWin = 0;
-    let computerWin = 0;
-    let input ;
-    for( let i = 0; i<5; i++){
-        input = prompt("choose your action");
-        announment = playRound(input,computerPlay())
-        console.log(announment);
-        if (announment.split(4,8) == "lose") computerWin++;
-        
-        else if (announment.split(4,7) == "win") playersWin++;
-
-    }
-    if(playersWin == computerWin) console.log("Its a draw!");
-    else if(playersWin > computerWin) console.log("You win!");
-    else if(playersWin == computerWin) console.log("You lose!");
-
-
-}
-
-
-game();
-
-
-
+let playerresult = 0;
+let computerresult = 0;
+const buttons = document.querySelectorAll('button');
+const h1 = document.querySelector('h1');
+const playerh3 = document.querySelector('#player_score');
+const computerh3 = document.querySelector('#computer_score');
+buttons.forEach(button => {
+        button.addEventListener("click", playRound);     
+    })
 
 
